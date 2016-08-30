0423dac383f2d7acf647d07a98e66ad40e1c75b8
FuZhenn <fuzhen@maptalks.org>
2016-08-31 01:08:31 +0800

    updates

--- index.js
+++ index.js
@@ -5,4 +5,4 @@ maptalks.Util.convertSVG = function (url, w, h, onComplete) {
     //use svg2img to convert svg to png.
     //https://github.com/FuZhenn/node-svg2img
     svg2img(url, {'width':w, 'height':h}, onComplete);
-}
+};
