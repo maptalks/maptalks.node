7fa1409f4bffb1955eb011400f5b6876b423ce99
FuZhenn <fuzhen@maptalks.org>
2016-08-31 01:01:43 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+var maptalks = require('maptalks'),
+    svg2img = require('svg2img');
+
+maptalks.Util.convertSVG = function (url, w, h, onComplete) {
+    //use svg2img to convert svg to png.
+    //https://github.com/FuZhenn/node-svg2img
+    svg2img(url, {'width':w, 'height':h}, onComplete);
+}
