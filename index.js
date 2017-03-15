cb9af0cce5fb5d7b303aa81f451c01b2142a1c08
fuzhenn <fuzhen@maptalks.org>
2017-03-15 18:18:03 +0800

    updates for maptalks ES6 version

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var maptalks = require('maptalks'),
     svg2img = require('svg2img');
 
-maptalks.Util.convertSVG = function (url, w, h, onComplete) {
+maptalks.Util.loadImage.svg = function (url, w, h, onComplete) {
     //use svg2img to convert svg to png.
-    //https://github.com/FuZhenn/node-svg2img
+    //https://github.com/fuzhenn/node-svg2img
     svg2img(url, {'width':w, 'height':h}, onComplete);
 };
